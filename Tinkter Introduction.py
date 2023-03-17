9dcb69fc0fe02e3a9de841473e2900884892dbee
Kanishk Israni <67801143+Kanishk20@users.noreply.github.com>
2023-03-17 23:14:58 +0530

    Add files via upload

---
+++ Tinkter Introduction.py
@@ -0,0 +1,5 @@
+from tkinter import *
+# widgets : GUI elements includes boxes , labels , images , textboxes 
+# windows : Contains all the widgets 
+window = Tk() # instansiating a window 
+window.mainloop()
